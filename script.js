57e394bf3eb985ff409915f7bcc531e00b3352b7
Adam Townsend <adam@desnudopenguino.com>
2024-05-09 10:31:24 -0700

    first round of updates
    + added some DOM to the html
    + added some script to the js to load random notes
    + added a little styling to the css

---
+++ script.js
@@ -0,0 +1,47 @@
+const notes = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#']
+let usedNoteKeys = []
+
+function selectNote(key) {
+	return notes[key]
+}
+
+function selectRandomUnusedNote() {
+	let randomKey = Math.floor(Math.random() * notes.length())
+	if(!usedNoteKeys.includes(randomKey)) {
+		usedNoteKeys.push(randomKey)
+		return selectNote()
+	}
+	return null
+}
+
+function showRandomUnusedNote() {
+	let canGetNote = true
+	let newNote = ''
+	if(12 == usedNoteKeys.length) {
+		usedNoteKeys = []
+	}
+	while(canGetNote) {
+		let randomNote = selectRandomUnusedNote()
+		if(null != randomNote) {
+			newNote = randomNote
+			canGetNote = false
+		}
+	}
+	document.getElementById('generatedNote').textContent = newNote
+	document.getElementById('current').textContent = usedNoteKeys.length
+	document.getElementById('total').textContent = notes.length
+}
+
+// capture space to start/continue the iteration
+
+document.getElementById('generateNote').onclick = (e) => {
+	showRandomUnusedNote()
+}
+
+// capture button press to start/continue iteration
+document.onkeypress = (e) => {
+	if('Space' == e.code) {
+		showRandomUnusedNote()
+	}
+}
+
